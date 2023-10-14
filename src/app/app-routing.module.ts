1a05059cc9ffc5b8213dda20966127bdd9c12614
Marlon Ramos <marlon_raoni10@hotmail.com>
2023-10-14 00:16:57 -0300

    Start of order implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,15 @@ import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { MessageComponent } from './components/message/message.component';
 import { CustomerComponent } from './components/customer/customer.component';
+import { OrderComponent } from './components/order/order.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'message', component: MessageComponent },
-  { path: 'customer', component: CustomerComponent }
+  { path: 'customer', component: CustomerComponent },
+  { path: 'order', component: OrderComponent }
 ];
 
 @NgModule({
--- src/app/components/order/order.component.ts
+++ src/app/components/order/order.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
 
+interface Item {
+  name: string;
+  price: number;
+  quantity: number;
+}
+
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.scss']
 })
 export class OrderComponent {
+  newItem: Item = {
+    name: '',
+    price: 0,
+    quantity: 0
+  };
 
+  order = {
+    items: [] as Item[]
+  };
+
+  constructor() {}
+
+  addItem() {
+    this.order.items.push({ ...this.newItem });
+    this.newItem = {
+      name: '',
+      price: 0,
+      quantity: 0
+    };
+  }
+
+  onSubmit() {
+    console.log('Pedido:', this.order);
+  }
 }
